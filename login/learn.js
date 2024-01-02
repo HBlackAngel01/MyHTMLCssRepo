ccd826459d14e05f08e2b9a6a6d70cc4519e4ae0
HBlackAngel01 <praveenpandian01@gmail.com>
2024-01-02 11:27:41 +0530

    newLoginPage uploaded by Praveen 02-01-2024

---
+++ login/learn.js
@@ -0,0 +1,7 @@
+function display(){
+    let a = document.getElementById("username").value;
+    let b = document.getElementById("password").value;
+    
+        alert("Username:" + a);
+        alert("Password:" + b);
+}
